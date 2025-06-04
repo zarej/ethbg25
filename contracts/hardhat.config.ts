8424c40cea6fb81f3e78260037aa40c2a489696b
Žare <zare@Milicas-MacBook-Pro.local>
2025-06-04 20:59:05 +0200

    try polkadot compile

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -16,20 +16,41 @@ const config: HardhatUserConfig = {
       accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
       chainId: 11155111,
     },
+    passet: {
+      url: "https://testnet-passet-hub-eth-rpc.polkadot.io",
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      chainId: 420420421,
+    },
   },
   solidity: {
     compilers: [
       {
         version: "0.8.28",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 999999,
-          },
+        // settings: {
+        //   optimizer: {
+        //     enabled: true,
+        //     runs: 999999,
+        //   },
+        // },
+      },
+    ],
+  },
+  etherscan: {
+    apiKey: {
+      passet: "empty",
+    },
+    customChains: [
+      {
+        network: "passet",
+        chainId: 4741,
+        urls: {
+          apiURL: "https://blockscout-passet-hub.parity-testnet.parity.io/api",
+          browserURL: "https://blockscout-passet-hub.parity-testnet.parity.io/",
         },
       },
     ],
   },
+
 };
 
 export default config;
